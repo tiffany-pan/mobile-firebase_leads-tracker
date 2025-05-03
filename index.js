9cb83c080f2eb54a84beeb2f449a9552cc29a608
Tiffany Pan <tfunnyblue@yahoo.com>
2025-05-03 13:48:06 -0400

    Remove localStorage; use Firebase

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-let myLeads = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: "https://leads-tracker-26c05-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);;
+const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
+
 const inputEl = document.getElementById('input-el');
-const tabBtn = document.getElementById('tab-btn');
 const inputBtn = document.getElementById('input-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
 
 function render(leads) {
   let listItems = "";
@@ -24,23 +26,23 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  inputEl.value = "";
-  render(myLeads);
+onValue(referenceInDB, function(snapshot) {
+  const snapshotDoesExist = snapshot.exists();
+  if (snapshotDoesExist) {
+    const data = snapshot.val();
+    const myLeads = Object.values(data);
+    render(myLeads);
+  } else {
+    console.log("No data available");
+  }
 });
 
-tabBtn.addEventListener("click", () => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    myLeads.push(tabs[0].url);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
-  });
+inputBtn.addEventListener("click", () => {
+  push(referenceInDB, inputEl.value);
+  inputEl.value = "";
 });
 
 deleteBtn.addEventListener("dblclick", () => {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
+  remove(referenceInDB);
+  ulEl.innerHTML = "";
 });
